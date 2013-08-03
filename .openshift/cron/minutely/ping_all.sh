039c921ad76b3f66f92d1a6e1bf9b997799c89f7
Kevin <kevin@koboyz.org>
2013-08-03 15:06:39 +0200

    Add logs during ping cron

--- .openshift/cron/minutely/ping_all.sh
+++ .openshift/cron/minutely/ping_all.sh
@@ -3,5 +3,7 @@
 if [ $(($(date +%M) % 15 == 0)) -eq 1 ]; then
   source $OPENSHIFT_DATA_DIR/.environment
   cd $OPENSHIFT_REPO_DIR
-  RAILS_ENV=production rake ping:all
+  echo "$(date +%D-%T) -- ping starting" >> ${OPENSHIFT_RUBY_LOG_DIR}/crontab
+  RAILS_ENV=production rake ping:all 2> ${OPENSHIFT_RUBY_LOG_DIR}/crontab
+  echo "$(date +%D-%T) -- ping finished" >> ${OPENSHIFT_RUBY_LOG_DIR}/crontab
 fi
